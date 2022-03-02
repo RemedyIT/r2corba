dae87b81e600120ae24bc4e10e9d21f1b97fb038
Johnny Willemsen <jwillemsen@remedy.nl>
2022-03-02 12:44:20 +0100

    Some more places to check for mingw instead of mingw32
        * lib/corba/cbase/require.rb:
        * lib/corba/svcs/ins/ins.rb:
        * scripts/package-prebuilt.rb:
        * scripts/rake-common.rb:
        * scripts/rake-gem.rb:
        * test/lib/test.rb:
        * test/test_runner.rb:

--- lib/corba/cbase/require.rb
+++ lib/corba/cbase/require.rb
@@ -13,7 +13,7 @@
 begin
   _ext_dir = File.expand_path(File.join(File.dirname(__FILE__), '../../../ext'))
   $: << _ext_dir unless $:.include?(_ext_dir) || !File.directory?(_ext_dir)
-  if RUBY_PLATFORM =~ /mingw32/
+  if RUBY_PLATFORM =~ /mingw/
     require 'ruby_installer/runtime'
     RubyInstaller::Runtime.add_dll_directory(_ext_dir)
     ENV['PATH'].split(File::PATH_SEPARATOR).each do |path|
